e885ce26a5a104759807c28b51a99c87bbcaf044
Anais Engelmajer <anaiscorinne@gmail.com>
2016-02-22 10:20:08 -0500

    Rake db migrate with everything

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,3 +1,51 @@
 class BrandsController < ApplicationController
-	before_action :admin_only, only: [:create, :destroy, :edit]
+	before_action :admin_only, only: [:create, :new, :destroy, :edit]
+
+	def index
+		@brands = Brand.all 
+	end
+
+	def new
+		@brand = Brand.new
+	end
+
+	def create
+		@brand = Brand.new(brand_params)
+			if @brand.save
+				link_to brands_path
+			else
+				render :new
+			end
+	end
+
+	def destroy
+	end
+
+	private
+	def brand_params
+		params.require(:brand).permit(:name, :description, :website, :instagram)
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160222000330) do
+ActiveRecord::Schema.define(version: 20160222144937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,52 @@ ActiveRecord::Schema.define(version: 20160222000330) do
     t.date     "date"
     t.text     "description"
     t.text     "content"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "brands", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "website"
+    t.string   "instagram"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "brandstores", force: :cascade do |t|
+    t.integer  "store_id"
+    t.integer  "brand_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "brandstores", ["brand_id"], name: "index_brandstores_on_brand_id", using: :btree
+  add_index "brandstores", ["store_id"], name: "index_brandstores_on_store_id", using: :btree
+
+  create_table "keybrands", force: :cascade do |t|
+    t.integer  "brand_id"
+    t.integer  "keyvalue_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "keybrands", ["brand_id"], name: "index_keybrands_on_brand_id", using: :btree
+  add_index "keybrands", ["keyvalue_id"], name: "index_keybrands_on_keyvalue_id", using: :btree
+
+  create_table "keyvalues", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "stores", force: :cascade do |t|
+    t.string   "name"
+    t.string   "website"
+    t.string   "instagram"
+    t.text     "description"
+    t.text     "address"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
@@ -48,4 +94,8 @@ ActiveRecord::Schema.define(version: 20160222000330) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "brandstores", "brands"
+  add_foreign_key "brandstores", "stores"
+  add_foreign_key "keybrands", "brands"
+  add_foreign_key "keybrands", "keyvalues"
 end
