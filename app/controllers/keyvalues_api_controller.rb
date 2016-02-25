6fd8a7155043371b4726f4acd191a1a1569796dd
Anais Engelmajer <anaiscorinne@gmail.com>
2016-02-24 19:05:29 -0500

    Layout for keyvalues including checkboxes

--- app/controllers/keyvalues_api_controller.rb
+++ app/controllers/keyvalues_api_controller.rb
@@ -6,7 +6,11 @@ class KeyvaluesApiController < ApplicationController
 		if params[:valueIds].nil? || params[:valueIds].empty?
 			brands= Brand.all
 		else
-			brands = Brand.joins(:keybrands).where(:keybrands => { :keyvalue_id => params[:valueIds] }).select("brands.*, COUNT(keyvalue_id)").group("brands.id").having("COUNT(keyvalue_id) >= ?", params[:valueIds].length)
+			brands = Brand.joins(:keybrands)
+				.where(:keybrands => { :keyvalue_id => params[:valueIds] })
+				.select("brands.*, COUNT(keyvalue_id)")
+				.group("brands.id")
+				.having("COUNT(keyvalue_id) >= ?", params[:valueIds].length)
 		end
 
 		render json: brands
