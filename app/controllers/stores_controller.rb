047afef8ce36e802e40ee29573ebb22feccb62e2
Anais Engelmajer <anaiscorinne@gmail.com>
2016-02-25 21:18:42 -0500

    Fix bug with geolocation sorting

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -2,21 +2,20 @@ class StoresController < ApplicationController
 	before_action :admin_only, only: [:create, :new, :destroy, :edit, :update]
 
 	def index
-	  @lat_lng = cookies[:lat_lng].split("|")
-		@stores = Store.all
 		@user = current_user
-
+		@lat_lng = cookies[:lat_lng].split("|")
+# if there is no geocode available then show all unsorted
 		if @lat_lng.nil?
 			@stores = Store.all
+# if not sort in order of closest to furthest
 		else
-			@stores = Store.within(5, :origin => @lat_lng).order('address DESC')
+			@stores = Store.by_distance(origin: @lat_lng)
 		end
 	end
 
 
 	def show
 		@store = Store.find_by(id: params[:id])
-		# render :show
 	end
 
 	def new
@@ -62,11 +61,15 @@ class StoresController < ApplicationController
 			store.brands.each do |brand|
 				if brand.name.include?(brand_name)
 				stores_included.push(store)
+				end
 			end
 		end
-	end
-	@stores = stores_included
-	render :by_brand
+		if @lat_lng.nil?
+			@stores = stores_included
+		else
+			@stores = stores_included.within(5, :origin => @lat_lng).order('address DESC')
+		end
+		render :by_brand
 	end
 
 	private
